01cff49235b037a7d5790940d06caf5c870ae4e4
germain <djuhngleen@gmail.com>
2022-05-23 21:49:45 +0200

    feat: create utils method generateCommunityCode

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,6 @@
+import { sample } from 'lodash';
+import { AlphabetEnum } from '@utils/enum/alphabet.enum';
+
 /**
  * @method isEmpty
  * @param {String | Number | Object} value
@@ -12,4 +15,24 @@ export const isEmpty = (value: string | number | object): boolean => {
   } else if (typeof value === 'undefined' || value === undefined) {
     return true;
   } else return value !== null && typeof value === 'object' && !Object.keys(value).length;
+};
+
+/**
+ * Create a random word from a string
+ * @param {String} base base string
+ * @param {number} length the length of the new word
+ */
+export const generateCommunityCode = (base: string, length = 2): string => {
+  const consonantsReg = new RegExp(`[${AlphabetEnum.CONSONANTS}]`, 'gi');
+  const vowelsReg = new RegExp(`[${AlphabetEnum.VOWELS}]`, 'gi');
+  let randomWord = '';
+
+  const baseConsonants = base.match(consonantsReg);
+  const baseVowels = base.match(vowelsReg);
+
+  for (let i = 0; i < length; i++) {
+    randomWord += sample(baseConsonants ? baseConsonants : AlphabetEnum.CONSONANTS) + sample(baseVowels ? baseVowels : AlphabetEnum.VOWELS);
+  }
+
+  return randomWord.toLowerCase();
 };
