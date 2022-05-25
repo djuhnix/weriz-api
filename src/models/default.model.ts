2b71950605f0b9b80c4fcf951c29ccd3daae83fa
germain <djuhngleen@gmail.com>
2022-05-25 02:33:50 +0200

    feat: add plugins to default model
    autopopulate & findOrCreate

--- src/models/default.model.ts
+++ src/models/default.model.ts
@@ -1,4 +1,10 @@
-class DefaultModel {
+import { defaultClasses, plugin } from '@typegoose/typegoose';
+import findOrCreate from 'mongoose-findorcreate';
+import autoPopulate from 'mongoose-autopopulate';
+
+@plugin(autoPopulate)
+@plugin(findOrCreate)
+class DefaultModel extends defaultClasses.FindOrCreate {
   public createdAt?: Date;
   public updatedAt?: Date;
 }
