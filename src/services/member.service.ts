6d163b12873bb141736a853d823d94773f8ded40
germain <djuhngleen@gmail.com>
2022-05-25 02:40:28 +0200

    feat: member.service

---
+++ src/services/member.service.ts
@@ -0,0 +1,64 @@
+import { MemberModel } from '@/models';
+import { CreateMemberDto } from '@dtos/member.dto';
+import { Member } from '@models/member.model';
+import { checkEmpty, checkObjectId } from '@utils/util';
+import { logger } from '@utils/logger';
+
+class MemberService {
+  private readonly _name = MemberService.name + '.';
+  private members = MemberModel;
+
+  public async findAllMember(filter: Partial<CreateMemberDto> = undefined): Promise<Member[]> {
+    return this.members.find(filter);
+  }
+
+  public async findMemberById(memberId: string): Promise<Member> {
+    logger.info(this._name + 'findMemberById.start');
+
+    checkObjectId(memberId);
+    const member: Member = await this.members.findOne({ _id: memberId });
+    checkEmpty(member, true);
+
+    logger.info(this._name + 'findMemberById.end');
+    return member;
+  }
+
+  public async createMember(memberData: CreateMemberDto): Promise<Member> {
+    logger.info(this._name + 'createMember.start');
+    checkEmpty(memberData);
+    checkObjectId(memberData.userId);
+
+    const result = await this.members.findOrCreate({
+      firstname: memberData.firstname,
+      lastname: memberData.lastname,
+      user: { _id: memberData.userId },
+    });
+    const member: Member = result.doc;
+
+    logger.info(`member ${result.created ? 'created' : 'found'}`, JSON.stringify(member));
+    logger.info(this._name + 'createMember.end');
+    return member;
+  }
+
+  public async updateMember(memberId: string, memberData: CreateMemberDto): Promise<Member> {
+    logger.info(this._name + 'updateMember.start');
+    checkEmpty(memberData);
+    checkObjectId(memberId);
+
+    const updatedMember: Member = await this.members.findByIdAndUpdate(memberId, { memberData });
+    checkEmpty(updatedMember, true);
+    logger.info(this._name + 'updateMember.end');
+    return updatedMember;
+  }
+
+  public async deleteMember(memberId: string): Promise<Member> {
+    logger.info(this._name + 'deleteMember.start');
+    checkObjectId(memberId);
+    const deletedMember: Member = await this.members.findByIdAndDelete(memberId);
+    checkEmpty(deletedMember, true);
+    logger.info(this._name + 'deleteMember.end');
+    return deletedMember;
+  }
+}
+
+export default MemberService;
