5959a124e520fb8a0dd6af53f26cebbc16a60999
germain <djuhngleen@gmail.com>
2022-05-25 03:15:36 +0200

    feat: update error.middleware : check error instance

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,11 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
 import { logger } from '@utils/logger';
 import { NODE_ENV } from '@config';
+import { isInstance } from 'class-validator';
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
   try {
     const status: number = error.status || 500;
-    const message: string = NODE_ENV == 'development' ? error.message || 'Something went wrong' : 'internal_error';
+    const message: string = isInstance(error, HttpException) ? error.message : 'internal_error';
     logger.error(`status = ${status}, message = ${message}`);
 
     res.status(status).json({ message, error: NODE_ENV == 'development' ? error : {} });
