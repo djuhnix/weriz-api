81716217788a1f40c1105735d31f2d420365e0d8
germain <djuhngleen@gmail.com>
2022-06-15 22:16:41 +0200

    feat: add GET members route

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UserRoute from '@routes/user.route';
 import validateEnv from '@utils/validateEnv';
+import MemberRoute from '@routes/member.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UserRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new UserRoute(), new MemberRoute()]);
 
 app.listen();
