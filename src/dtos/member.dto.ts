60202f5f4cef54db11993d5bb283d59c64b57cc9
germain <djuhngleen@gmail.com>
2022-06-15 11:36:03 +0200

    feat: add GetMemberDto

--- src/dtos/member.dto.ts
+++ src/dtos/member.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateMemberDto {
   @IsString()
@@ -11,5 +11,23 @@ export class CreateMemberDto {
   public userId: string;
 
   @IsString()
+  public communityCode?: string;
+}
+
+export class GetMemberDto {
+  @IsString()
+  @IsOptional()
+  public firstname? = '';
+
+  @IsString()
+  @IsOptional()
+  public lastname? = '';
+
+  @IsNotEmpty()
+  @IsOptional()
+  public userId?: string;
+
+  @IsString()
+  @IsOptional()
   public communityCode?: string;
 }
