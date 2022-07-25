dacd9fa3f9267ef8887390b7f4d1735bddf3b54d
djuhnix <djuhngleen@gmail.com>
2022-07-25 13:15:18 +0200

    feat: update database env var to app config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,3 @@
-import { DB_DATABASE, DB_HOST, DB_PORT } from './';
+import { DB_DATABASE, DB_HOST, DB_PORT, DB_USER, DB_PASS } from './';
 
-export const dbConnection = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+export const dbConnection = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
